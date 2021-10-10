54c1a1d0496f7c4c9972acc1e71a38f977c92a29
Anatolii-sx <s8.8@bk.ru>
2021-10-10 11:30:59 +0300

    Initial Commit

---
+++ SimpleToDoListWithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleToDoListWithCoreData
+//
+//  Created by Анатолий Миронов on 10.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
